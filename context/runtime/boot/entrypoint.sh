cbb9ecb9813a4fd0682d337cc5fb8414fc091288
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-10-18 18:40:56 -0700

    Bump
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

---
+++ context/runtime/boot/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -o errexit -o errtrace -o functrace -o nounset -o pipefail
+
+root="$(cd "$(dirname "${BASH_SOURCE[0]:-$PWD}")" 2>/dev/null 1>&2 && pwd)"
+readonly root
+# shellcheck source=/dev/null
+source "$root/helpers.sh"
+# shellcheck source=/dev/null
+source "$root/mdns.sh"
+
+helpers::dir::writable /data
+
+# mDNS blast if asked to
+[ ! "$MDNS_HOST" ] || {
+  [ ! "${MDNS_STATION:-}" ] || mdns::add "_workstation._tcp" "$MDNS_HOST" "${MDNS_NAME:-}" "$PORT"
+  mdns::add "${MDNS_TYPE:-_http._tcp}" "$MDNS_HOST" "${MDNS_NAME:-}" "$PORT"
+  mdns::start &
+}
+
+if [ "${USERNAME:-}" ]; then
+  export REGISTRY_AUTH=htpasswd
+  export REGISTRY_AUTH_HTPASSWD_REALM="$REALM"
+  export REGISTRY_AUTH_HTPASSWD_PATH=/data/htpasswd
+  printf "%s:%s\n" "$USERNAME" "$(printf "%s" "$PASSWORD" | base64 -d)" > /data/htpasswd
+fi
+
+# args=()
+
+# Run once configured
+exec node parse-server
